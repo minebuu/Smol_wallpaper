687b30a2ccae4a4a1985484a9b322e7d256814a1
Minebuu <minebuu.eth@gmail.com>
2022-08-26 16:34:24 +0900

    Update create_wallpaper.py

--- create_wallpaper.py
+++ create_wallpaper.py
@@ -10,9 +10,7 @@ nft = ['smol', 'pet', 'swol' 'legion', 'gl']
 
 phone_dic = {'i12':(1170, 2532),'i12mini':[1125,2436],'i12pro':[1170, 2532],'i12max':[1284,2778], 'i13':[1170,2532], 'i13mini':[1080,2340], 'i13pro':[1170,2532],'i13max':[1284,2778], 'gflip3':[2520, 2520], 'gflip3_cover':[2260, 2260]}
 
-nft_url = {'smol_male': "https://ipfs.io/ipfs/QmY71ban6QoWg9nbNwikk6wVWknj8NFBG8nMGHEuzwfAwf/", 'smol_female': "https://gateway.pinata.cloud/ipfs/QmR87K1oY8dXL4op91A9zcz4hPmCd8JbMVDTTuUnpXyQcr/", 'brain_pet': "https://ipfs.io/ipfs/QmdRyjjv6suTcS9E1aNnKRhvL2McYynrzLbg5VwXH8cCQB/"}
-
-nft_url_local = {'smol_male': "http://bafybeiercznqjdejbvccbp4byylolmlgybsivp433qx77fkqzyt3s67ccy.ipfs.localhost:8080/", 'smol_female': "https://gateway.pinata.cloud/ipfs/QmR87K1oY8dXL4op91A9zcz4hPmCd8JbMVDTTuUnpXyQcr/", 'brain_pet': "https://ipfs.io/ipfs/QmdRyjjv6suTcS9E1aNnKRhvL2McYynrzLbg5VwXH8cCQB/"}
+nft_url = {'smol_male': "https://minebuu.mypinata.cloud/ipfs/QmY71ban6QoWg9nbNwikk6wVWknj8NFBG8nMGHEuzwfAwf/", 'smol_female': "https://minebuu.mypinata.cloud/ipfs/QmR87K1oY8dXL4op91A9zcz4hPmCd8JbMVDTTuUnpXyQcr/", 'brain_pet': "https://minebuu.mypinata.cloud/ipfs/QmdRyjjv6suTcS9E1aNnKRhvL2McYynrzLbg5VwXH8cCQB/"}
 
 def create_smol_wp(file_name, device, nft_loc, nft_size):
 	w = phone_dic[device][0]
@@ -62,47 +60,54 @@ def get_nft_url(nft_name, id, head_size):
 	if nft_name == "smol":
 		if id <= LAST_MALE:
 			# url = nft_url["smol_male"] + str(id) + "/" + str(head_size) + ".png"
-			url = nft_url_local["smol_male"] + str(id) + "/" + str(head_size) + ".png"
+			url = nft_url["smol_male"] + str(id) + "/" + str(head_size) + ".png"
 		elif id <= LAST_FEMALE:
 			id = id - LAST_MALE - 1 #Female starts 0 index
 			url = nft_url["smol_female"] + str(id) + "/" + str(head_size) + ".png"
 	elif nft_name == "brain_pet":
 		url = nft_url[nft_name] + str(id) + ".gif"
+	else:
+		return 0
 	print(url)
-	return url;
+	return url
 
 def save_nft_image(nft_name, id, head_size):
 	url = get_nft_url(nft_name, id, head_size)
 	#first check saved image
 	if nft_name == "smol":
 		file_name = nft_name + "_" + str(id) + "_" + str(head_size) + ".png"
-		if os.path.exists(file_name):
+		if os.path.exists("./images/smol/" + file_name):
 			print(file_name + " is already exist")
 		else:
-			urllib.request.urlretrieve(url, file_name)
-			img.save("./images/smol/" + file_name,'png', optimize=True, quality=100)
+			opener = urllib.request.build_opener()
+			opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+			urllib.request.install_opener(opener)
+			urllib.request.urlretrieve(url, "./images/smol/"  + file_name)
 
 	elif nft_name == "brain_pet":
 		file_name = nft_name + "_" + str(id) + ".png"
 		file_gif = nft_name + "_" + str(id) + ".gif"
-		if os.path.exists(file_name):
+		if os.path.exists("./images/brainPets/" + file_name):
 			print(file_name + " is already exist")
 		else:
-			urllib.request.urlretrieve(url, file_gif)
-			img = Image.open(file_gif)
-			img.save("./images/brain_pets/" + file_name,'png', optimize=True, quality=100)
+			opener = urllib.request.build_opener()
+			opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+			urllib.request.install_opener(opener)
+			urllib.request.urlretrieve(url, "./images/brainPets/"  + file_gif)
+			img = Image.open("./images/brainPets/" + file_gif)
+			img.save("./images/brainPets/" + file_name,'png', optimize=True, quality=100)
 	return file_name
 
 def wp(device, nft_name, id, head_size):
 	file_name = save_nft_image(nft_name, id, head_size)
 	create_smol_wp(file_name, device, "low", "L")
 
-# wp("i12pro","smol",710,5)
-wp("i12pro","smol",6710,5)
-# wp("i12pro","smol",6711,5)
+for id in range(LAST_FEMALE):
+	for size in range(5):
+		# save_nft_image("smol",id, size)
+
+
 # wp("i12pro","smol",6752,5)
-
-
 # wp("gflip3_cover","pet",770,5)
 # get_ipfs_url("smol",3553,5)
 # wp("i12pro","tool",384,5)
